f8f2db4d9408492789c4ff5c0da9bef6c58f2792
Eric Anderson <eric.anderson4@hp.com>
2007-09-06 00:01:42 +0000

    * Add support for not using lintel statistics to calculate the stats,      faster, but much less general.

--- src/paper/srtstat2.C
+++ src/paper/srtstat2.C
@@ -13,7 +13,30 @@ srtstat2 bytes device_number bytes logical_volume_number bytes machine_id --
 
 */
 
+/* Performance notes:
+   2 dual core Intel 3GhZ Xeon processors (4 cores total), 5GB memory
+
+(LINTEL_STATS)
+time srtstat2 bytes device_number -- select-bytes-dev.none.ds  >/tmp/f
+real    0m8.523s user    0m9.509s sys     0m2.512s
+real    0m8.568s user    0m9.573s sys     0m2.532s
+real    0m8.418s user    0m9.501s sys     0m2.744s
+
+(DIRECT_STATS)
+time srtstat2 bytes device_number -- select-bytes-dev.none.ds  >/tmp/f
+real    0m5.554s user    0m6.444s sys     0m2.680s
+real    0m5.609s user    0m6.268s sys     0m2.624s
+real    0m5.477s user    0m6.500s sys     0m2.764s
+
+time srtstat2 bytes device_number -- select-bytes-dev.lzo.ds  >/tmp/f
+real    0m5.428s user    0m7.020s sys     0m1.188s
+real    0m5.438s user    0m7.100s sys     0m1.056s
+real    0m5.407s user    0m6.980s sys     0m1.188s
+
+*/
+
 #include <string>
+#include <malloc.h>
 
 #include <boost/format.hpp>
 
@@ -34,6 +57,10 @@ struct HashMap_hashintfast {
     }
 };
 
+#define DIRECT_STATS 1
+#define LINTEL_STATS 0
+
+int foo;
 class IntGroupByInt : public RowAnalysisModule {
 public:
     IntGroupByInt(DataSeriesModule &source, 
@@ -46,29 +73,91 @@ public:
     {
     }
 
-    typedef HashMap<ExtentType::int32, Stats *, HashMap_hashintfast> mytableT;
+    struct GStats {
+#if DIRECT_STATS
+      double sum;
+      double count;
+#endif
+#if LINTEL_STATS
+      Stats *stat;
+#endif
+      GStats() {
+#if DIRECT_STATS
+	sum = count = 0;
+#endif
+#if LINTEL_STATS
+	stat = new Stats();
+	// ++foo; INVARIANT(foo < 100000, "bad");
+#endif
+      }
+      void add(double v) {
+#if DIRECT_STATS
+	sum += v;
+	count += 1;
+#endif
+#if LINTEL_STATS
+	stat->add(v);
+#endif
+      }
+    };
+    typedef HashMap<ExtentType::int32, GStats *, HashMap_hashintfast> mytableT;
 
     virtual ~IntGroupByInt() { }
     
     virtual void processRow() {
-	Stats *stat = mystats[groupby.val()];
-	if (stat == NULL) {
-	    stat = new Stats();
+	GStats **statp = mystats.lookup(groupby.val());
+	GStats *stat;
+	if (statp == NULL) {
+	    stat = new GStats();
 	    mystats[groupby.val()] = stat;
+	} else {
+	  stat = *statp;
 	}
 	stat->add(value.val());
     }
 
+    virtual Extent *getExtent() {
+      Extent *e = source.getExtent();
+
+      if (e == NULL) {
+	completeProcessing();
+	return NULL;
+      }
+      series.setExtent(e);
+
+      if (!prepared) {
+	prepareForProcessing();
+	prepared = true;
+      }
+      for(;series.pos.morerecords();++series.pos) {
+	processRow();
+      }
+      return e;
+    }
+
     virtual void printResult() {
+#if DIRECT_STATS
+	cout << boost::format("%s, mean(%s)\n")
+	    % groupby_field % value_field;
+	for(mytableT::iterator i = mystats.begin(); 
+	    i != mystats.end(); ++i) {
+	    cout << boost::format("%d, %.15g\n") 
+	      % static_cast<int32_t>(i->first) // % i->second->count
+	      % (i->second->sum / i->second->count);
+	}
+#endif
+
+#if LINTEL_STATS
 	cout << boost::format("%s, count(*), mean(%s), stddev, min, max\n")
 	    % groupby_field % value_field;
 	for(mytableT::iterator i = mystats.begin(); 
 	    i != mystats.end(); ++i) {
-	    cout << boost::format("%u, %d, %.6g, %.6g, %.6g, %.6g\n") 
-		% static_cast<uint32_t>(i->first) % i->second->count() 
-		% i->second->mean() % i->second->stddev()
-		% i->second->min() % i->second->max();
+	    cout << boost::format("%d, %d, %.6g, %.6g, %.6g, %.6g\n") 
+		% static_cast<int32_t>(i->first) % i->second->stat->count() 
+		% i->second->stat->mean() % i->second->stat->stddev()
+		% i->second->stat->min() % i->second->stat->max();
 	}
+#endif
     }
 
     mytableT mystats;
@@ -82,6 +171,9 @@ main(int argc, char *argv[])
     TypeIndexModule source("Trace::BlockIO::SRT");
     PrefetchBufferModule *prefetch = new PrefetchBufferModule(source,64*1024*1024);
 
+    mallopt(M_MMAP_THRESHOLD, 1024 * 1024);
+    mallopt(M_TRIM_THRESHOLD, 1024 * 1024);
+    mallopt(M_MMAP_MAX, 0);
     SequenceModule seq(prefetch);
     int argpos = 1;
     for(argpos = 1; argpos < argc; ) {
@@ -100,7 +192,7 @@ main(int argc, char *argv[])
     }
     DataSeriesModule::getAndDelete(seq);
     
-    RowAnalysisModule::printAllResults(seq,1);
+    RowAnalysisModule::printAllResults(seq);
 
     printf("extents: %.2f MB -> %.2f MB in %.2f secs decode time\n",
 	   (double)(source.total_compressed_bytes)/(1024.0*1024),
