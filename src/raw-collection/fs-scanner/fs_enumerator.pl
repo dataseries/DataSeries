af31512972c316eeb095cea4e3e45a88be48a4f3
Eric Anderson <eric.anderson4@hp.com>
2007-09-27 02:38:51 +0000

    * Lots of updates to get these working in a production environment.
    fs_enumerator.pl: Fix copyright.  Remove dependency on lzf.  Rewrite the
      processing so that we don't mount everything on the first pass, but instead
      we only touch directories when we need them.  Furthermore, we make sure
      to wait for up to 60 seconds for a directory to appear to deal with 
      any automounter issues.  Finally, skip netapp .snapshot directories, no
      real reason to scan them.
    
    hostpinger.pl: Fix copyright.  Add in a cache for ping times and directories
      found.  Add signal handler for graceful shutdown.  Improve input validation
      Stop using Net::Ping, but use the ping command directly as we need to do
      ICMP not tcp echo pinging since some machines may not support tcp echo.
      Lots of additional complexity in an effort to deal with automounter not
      being able to handle lots of mounts at the same time.  In practice this
      amounts to running multiple commands to mount directories, and checking
      multiple times to see if things exist, and in the worst case, running 
      the program multiple times.  The cache means that once it has established
      something exists, it doesn't need to check again.

--- src/raw-collection/fs-scanner/fs_enumerator.pl
+++ src/raw-collection/fs-scanner/fs_enumerator.pl
@@ -1,17 +1,9 @@
-#*****************************************************************************
-#*
-#* File:         fs_enumerator.pl
-#* Description:  File system directory scanner
-#* Author:       Brad Morrey
-#* Created:      Thu Jun 28 18:04:20 PDT 2007
-#* Modified:     Thu Jul 23 18:04:20 PDT 2007 (Brad Morrey) brad.morrey@hpl.hp.com
-#* Language:     Perl
-#* Package:      N/A
-#* Status:       Experimental (Do Not Distribute)
-#*
-#* (C) Copyright 2007, Hewlett-Packard Laboratories, all rights reserved.
-#*
-#****************************************************************************
+#!/usr/bin/perl -w
+#   (c) Copyright 2007, Hewlett-Packard Development Company, LP
+#
+#   See the file named COPYING for license details
+#
+# Description: File system directory scanner
 
 #This is MESSY CODE Designed to scan a set of directories and store
 #all of the metadata in rotating logfiles defined by $outFileSpec If
@@ -19,94 +11,126 @@
 #prunes proc, dev, sys subtrees because their metadata does not seem
 #to be valid and screws up the file system size totals 
 #
-#Requires: lzf 
+# TODO: clean this up ;)
+# 
 #TODO: Make this work with BatchParallel, with configurable
 #compression, and with configurable log rotation size
 
-
 use File::Find;
 use MIME::Base64;
-$maxDepth = 1;
-$curDepth = 0;
-$entryRotateCount = 1000000;
-if ($#ARGV < 1) {
-    print "USAGE: perl fs_enumerator.pl OUTFILESPEC DIR|FILE [DIR|FILE ...]\n";
-    print "OUTFILESPEC is the prefix for all output files\n";
-    print "DIR is one or more directories to be scanned in order\n";
-    print "FILE is one or more files that contain a list of directories to be scanned\n";
+use strict;
+
+my $maxDepth = 1;
+my $curDepth = 0;
+my $entryRotateCount = 1000000;
+usage() unless @ARGV > 0 || $ARGV[0] =~ /^-h/o;
+usage("outfile-prefix should not exist") if -f $ARGV[0];
+
+sub usage {
+    print "Usage: $0 outfile-prefix (dir|file)...\n";
+    print "outfile-prefix is the prefix for all output files\n";
+    print "dir is one or more directories to be scanned in order\n";
+    print "file is one or more files that contain a list of directories to be scanned\n";
+    die @_ if @_ > 0;
     exit(0);
 }
-@dirList = @ARGV[1..$#ARGV];
-$outFileSpec = $ARGV[0];
-print "argv[1] is @dirList and ARGV[0] is $outFileSpec\n";
-print "startdir 0 is $dirList[0] 1 is $dirList[1]\n";
+my $outFileSpec = shift @ARGV;
+my @pending = @ARGV;
 
-$argDirCount = 0;
-$outFileCurCount = 0;
-@ver_dir_list = ();
-foreach $curDir (@dirList) {
-    if (-f $curDir) {
-	# $curDir is not a directory but a FILE
-	open (FILEOFDIRS, $curDir) || die "couldn't open $curDir ($!)";
-	while (defined ($cur_dir_line = <FILEOFDIRS>)) {
-	    chomp($cur_dir_line);
-	    dirVerify($cur_dir_line);
+# print "argv[1] is @dirList and ARGV[0] is $outFileSpec\n";
+# print "startdir 0 is $dirList[0] 1 is $dirList[1]\n";
+
+my $processed_dir_count = 0;
+my $outFileSpecComplete;
+my $outFileEntryCount = 0;
+my $outFileCurCount = 0;
+
+my $baseDirectory;
+my $totalSize = 0;
+my $totalCount = 0;
+my $curSize = 0;
+
+while (@pending > 0) {
+    my $thing = shift @pending;
+    waitFor($thing);
+    if (-f $thing) {
+ 	open (FILEOFDIRS, $thing) || die "couldn't open $thing ($!)";
+ 	while (<FILEOFDIRS>) {
+ 	    chomp;
+	    push(@pending, $_);
+	}
+	close(FILEOFDIRS);
+    } elsif (-d $thing) {
+	my $dirCopy = $thing;
+	$dirCopy =~ s!/!_!go;
+	$outFileSpecComplete = "$outFileSpec-$dirCopy";
+	if (-f "$outFileSpecComplete-done") {
+	    print "Already scanned $thing\n";
+	} else {
+	    print "logging to $outFileSpecComplete\n";
+	    $outFileCurCount = 0;
+	    rotateLogs();
+
+	    $totalSize = $totalCount = $curSize = $outFileEntryCount = 0;
+	    $baseDirectory = $thing;
+	    find(\&wanted, $thing);
+	    print STDERR "done with \#$processed_dir_count: $thing\n";
+	    ++$processed_dir_count;
+	    close(OUTFILE);
+	    open(FOO, ">$outFileSpecComplete-done")
+		or die "boo";
+	    close(FOO);
 	}
     } else {
-	dirVerify($curDir);
+	die "neither a directory nor file be: $thing";
     }
 }
 
-foreach $curDir (@ver_dir_list) {
-    $dirCopy = $curDir;
-    $dirCopy =~ s/\//\_/g;
-    print "$dirCopy\n";
-    $outFileSpecComplete = "$outFileSpec-$dirCopy";
-    rotateLogs();
-    find(\&wanted, $curDir);
-    print (STDERR "done $argDirCount of $#dirList $curDir\n");
-    $argDirCount++;
-    close(OUTFILE);
-    $outFileCurCount = 0;
-    $outFileEntryCount = 0;
-}
+sub waitFor {
+    my($what) = @_;
 
-sub dirVerify {
-    $curDir = $_[0];
-    if (-e $curDir && -d $curDir) {
-	push @ver_dir_list, $curDir;
-    } else {
-	print "found a non dir $curDir\n";
+    print "waitFor $what: ";
+    for(my $i = 0; $i < 60; ++$i) {
+	last if -d $what || -f $what;
+	print ".";
+	sleep(1);
     }
+    print "ok." if -d $what || -f $what;
+    print "\n";
 }
 
 sub rotateLogs {
     close(OUTFILE);
-    $outFileName = "|lzf -c > $outFileSpecComplete-$outFileCurCount.lzf";
+    my $compressor = "gzip -1 -c";
+    my $outFileName = "| $compressor > $outFileSpecComplete-$outFileCurCount.gz";
     open(OUTFILE, $outFileName) or die "couldn't open $outFileName ($!)";
     $outFileCurCount++;
 }
 
-$totalSize = 0;
-$curSize = 0;
 sub wanted {
     #print "$_\n";
-    ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,
-     $atime,$mtime,$ctime,$blksize,$blocks) = stat $_;
-    $b64Name = encode_base64($_, "")|| die "can't encode $_ because $!";
-    $b64NameLength = length $b64Name;
-    $b64Dir = encode_base64($File::Find::dir, "")|| die "can't encode $File::Find::dir because $!";
-    $b64DirLength = length $b64Dir;
-    $printString = "$b64DirLength $b64NameLength $b64Dir $b64Name $dev $ino $mode $nlink $uid $gid $rdev $size $atime $mtime $ctime $blksize $blocks\n";
-    print OUTFILE $printString;
+#    my ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,
+#     $atime,$mtime,$ctime,$blksize,$blocks) = stat $_;
+    my @statbits = lstat $_;
+    die "?? $File::Find::dir $_" unless @statbits == 13;
+    my $b64Name = encode_base64($_, "")|| die "can't encode $_ because $!";
+    my $b64NameLength = length $b64Name;
+    my $b64Dir = encode_base64($File::Find::dir, "")|| die "can't encode $File::Find::dir because $!";
+    my $b64DirLength = length $b64Dir;
+    my $printString = join(" ",$b64DirLength,$b64NameLength, $b64Dir, $b64Name, @statbits);
+
+    my $size = $statbits[7];
+    print OUTFILE "$printString\n";
     #print "$File::Find::dir $_ $dev $ino $mode $nlink $uid $gid $rdev $size $atime $mtime $ctime $blksize $blocks\n";
+    ++$totalCount;
     if (-f $_) {
 	$curSize += $size;
 	if ($curSize > (500*1024*1024)) {
 	    $totalSize += $curSize;
 	    $curSize = 0;
-	    $printCount = $totalSize/(1024*1024*1024);
-	    print (STDERR "$printCount GB $File::Find::dir $_\n");
+	    my $printCount = $totalSize/(1024*1024*1024);
+	    printf STDERR "%.3f GiB, %.3f Mfiles; $File::Find::name\n",
+	       $totalSize/(1024*1024*1024), $totalCount/(1000*1000);
 	}
     }
     $outFileEntryCount++;
@@ -116,12 +140,14 @@ sub wanted {
 	$outFileEntryCount = 0;
     }
     #Stuff to skip here -----
-    if ($File::Find::dir =~ /^\/$/) {
-	print STDERR "in root dir\n";
-	if ($_ =~ /^proc$/ || $_ =~ /^dev$/ || $_ =~ /^sys$/) {
+    if ($File::Find::dir eq $baseDirectory) {
+	if ($File::Find::dir eq '/' &&
+	    /^proc$/o || /^dev$/o || /^sys$/o) {
+	    print STDERR "Prune -- /{proc,dev,sys}\n";
 	    $File::Find::prune = 1;
-	    print STDERR "PRUNING!\n";
+	} elsif (/^\.snapshot$/o) {
+	    print STDERR "Prune -- netapp .snapshot directory\n";
+	    $File::Find::prune = 1;
 	}
     }
-    #print "\n";
 }
--- src/raw-collection/fs-scanner/hostpinger.pl
+++ src/raw-collection/fs-scanner/hostpinger.pl
@@ -1,46 +1,212 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+#   (c) Copyright 2007, Hewlett-Packard Development Company, LP
+#
+#   See the file named COPYING for license details
+#
+# Description: Determine which set of hosts are nearby such that 
+# we will only use the fs_enumerator on the "nearby" fileservers.
 
-use Net::Ping;
+use strict;
+use AnyDBM_File;
+use FileHandle;
 
-# USAGE: perl hostpinger.pl INFILE OUTFILE
-# INFILE is the name of the file that contains directory hostname pairs
-# one per line
-# OUTFILE is the name of the file that will contain all directories 
-# for which the associated hostname in the infile is pingable in less
-# than 1ms.  Uses a Hashtable to limit ping traffic.
+my %hostpinger_cache;
+tie(%hostpinger_cache, 'AnyDBM_File', "hostpinger-cache", O_CREAT|O_RDWR, 0644) 
+    || die "can't create hostpinger-cache: $!\n";
 
-print "file to process: $ARGV[0]\n";
-open (DIRHOST, $ARGV[0]) or die "couldnt' open $ARGV[0] ($!)";
-open (OUTDIRS, ">$ARGV[1]") or die "couldnt' open $ARGV[1] ($!)";
-while (defined ($cur_dir_host = <DIRHOST>)) {
+my $stop = 0;
+$SIG{INT} = \&stop;
+
+sub stop {
+    ++$stop;
+    die "force die" if $stop > 3;
+    $SIG{INT} = \&stop;
+};
+
+$|=1;
+
+sub usage {
+    print "Usage: $0 infile outfile\n";
+    print "Infile contains lines with a directory and hostname on them.\n";
+    print "Outfile will contain all of the directories for which the\n";
+    print "associated hostname in the infile is pingable in less than 1 ms.\n";
+    print "Pings each host exactly one time.\n";
+    die @_ if @_;
+    exit(0);
+}
+
+my $max_ping_ms = 1;
+
+usage() if @ARGV == 0 || $ARGV[0] =~ /^-h/o;
+usage("incorrect number of arguments") unless @ARGV == 2;
+
+my ($infile,$outfile) = @ARGV;
+usage("infile '$infile' does not exist") unless -f $infile;
+usage("outfile '$outfile' already exists") if -f $outfile;
+
+print "reading from $infile, writing to $outfile.\n";
+open (DIRHOST, $infile) or die "could not open $infile: $!";
+open (OUTDIRS, ">$outfile") or die "could not open $outfile: $!";
+
+my @dir_recheck;
+my %host_map;
+while (defined (my $cur_dir_host = <DIRHOST>)) {
     chomp($cur_dir_host);
     #print "cur line: $cur_dir_host\n";
-    @split_it_up = split /\s+/, $cur_dir_host;
+    my @split_it_up = split /\s+/, $cur_dir_host;
+
+    if (@split_it_up == 3) {
+	die "?? $cur_dir_host" unless $split_it_up[2] =~ /^$split_it_up[1]/;
+	pop @split_it_up;
+    }
+    die "?? $cur_dir_host"
+	unless @split_it_up == 2;
+
+    my($cur_dir, $cur_host) = @split_it_up;
+
     #print "count $#split_it_up\n";
     #foreach $foo (@split_it_up) {
 #	print "cur $foo\n";
 #    }
-    $cur_dir = $split_it_up[0];
-    $cur_host = $split_it_up[1];
     
-    $host_ping = $host_map{$cur_host};
-    print "dir $cur_dir host $cur_host\n";
+    my $host_ping = $host_map{$cur_host};
+    print "host $cur_host: ";
+
+    unless(defined $host_ping) {
+	$host_ping = $hostpinger_cache{"ping: $cur_host"};
+    }
 
     unless (defined $host_ping) {
-	$p = Net::Ping->new();
-	$p->hires();
-	($ret, $host_ping, $ip) = $p->ping($cur_host, 5.5);
-	printf("$host [ip: $ip] is alive (packet return time: %.2f ms)\n", 1000 * $host_ping)
-	    if $ret;
-	$p->close();
+	my $cmd = "ping -c 3 -i 0.5 -w 5.5 $cur_host";
+	print "ping $cur_host: ";
+
+	open(PING, "$cmd 2>&1 |")
+	    or die "can't run $cmd: $!";
+
+	my ($ret, $ip);
+	$_ = <PING>;
+	if (/^PING (\S+) \((\d+\.\d+\.\d+\.\d+)\) \d+\(\d+\) bytes of data.$/o) {
+	    $ip = $2;
+	    print " (ip = $ip)";
+	} elsif (/^ping: unknown host/o) {
+	    print " (unknown host)";
+	} else {
+	    die "?? $_";
+	}
+
+	if (defined $ip) {
+	    while(<PING>) {
+		if (m!^rtt min/avg/max/mdev = (\d+\.\d+)/\d+\.\d+!o) {
+		    $host_ping = $1;
+		    print " (ping time $host_ping)";
+		}
+		if (m!(\d+) received!o) {
+		    print " (ok)";
+		    $ret = 1 if $1 > 0;
+		}
+	    }
+	}
+	close(PING);
+	if ($? == 0) {
+	    die "ping ok, output not??" unless $ret;
+	} else {
+	    die "output ok, ping not??" if $ret;
+	}
+
+	if ($ret) {
+	    print " alive\n";
+	} else {
+	    print " dead\n";
+	}
+	$host_ping = 1e6 unless $ret;
 	$host_map{$cur_host} = $host_ping;
+	$hostpinger_cache{"ping: $cur_host"} = $host_ping;
     }
-    if ($host_ping * 1000 < 1) {
-	print OUTDIRS $cur_dir;
-	print OUTDIRS "\n";
+
+    if ($host_ping < $max_ping_ms) {
+	print "fast";
+    } else {
+	print "slow; skipping\n";
+	next;
+    }
+
+    print " $cur_dir: ";
+
+    if ($hostpinger_cache{"directory: $cur_dir"}) {
+	print "cached-exists\n";
+    } else {
+	print "slow-check...";
+	
+	unless (testdir($cur_dir)) {
+	    $hostpinger_cache{"fail-exists: $cur_dir"} ||= 0;
+	    if ($hostpinger_cache{"fail-exists: $cur_dir"} >= 5) {
+		print "Failed at least 5 times, assuming doesn't exist.\n";
+		next;
+	    }
+	    # Test -d sometimes doesn't work with the automounter
+	    my $dir_exists = 
+		system("ls -d $cur_dir >/dev/null 2>/dev/null") == 0;
+	    select(undef,undef,undef,0.5)
+		unless $dir_exists;
+	    
+	    unless (-d $cur_dir) {
+		if (-e $cur_dir) {
+		    system("ls -ld $cur_dir");
+		    die "?? ($dir_exists) $cur_dir; try running it again, pinger cache may fix this :(";
+		}
+		print "skipping $cur_dir; it does not exist\n";
+		# Can't cache this case because the automounter might find it
+		# next time.
+		$hostpinger_cache{"fail-exists: $cur_dir"} += 1;
+		push(@dir_recheck, $cur_dir);
+		next;
+	    }
+	    print "ok, it exists.\n";
+	    die "??" unless testdir($cur_dir);
+	}
+	print "slow found.\n";
+    }
+
+    print OUTDIRS "$cur_dir\n";
+    if ($stop) {
+	untie(%hostpinger_cache);
+	print "Graceful shutdown\n";
+        exit(0);
     }
 }
 
-foreach $bar (keys %host_map) {
-    print "key $bar value $host_map{$bar}\n";
+if (@dir_recheck > 0) {
+    foreach my $dir (@dir_recheck) {
+	testdir($dir);
+    }
+    my $n = @dir_recheck;
+    print "Long wait for re-test of $n directories\n";
+    sleep(10);
 }
+
+foreach my $dir (@dir_recheck) {
+    next if $hostpinger_cache{"directory: $dir"};
+    print "Recheck $dir\n";
+    if (testdir($dir)) {
+	die "directory $dir appeared; run me again.\n";
+    }
+}
+
+foreach my $host (sort keys %host_map) {
+    print "host $host ping time $host_map{$host}ms\n";
+}
+
+sub testdir {
+    my($dir) = @_;
+
+    return 1 if $hostpinger_cache{"directory: $dir"};
+
+    if (-d $dir) {
+	delete $hostpinger_cache{"fail-exists: $dir"};
+	$hostpinger_cache{"directory: $dir"} = 1;
+	return 1;
+    } else {
+	return 0;
+    }
+}
+    
