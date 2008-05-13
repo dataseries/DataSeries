ce15293647b87452f80ca267f9ef3a78aa0260ab
Brad Morrey <brad.morrey@hp.com>
2008-05-13 00:59:54 +0000

    src/tests/CMakeLists.txt:lsfdsplots requires perl_crypt_rijndael which   was not being tested for in cmake.
    src/tests/run-check-dsstatgroupby.sh:Remove spurious
      ~/projects/DataSeries from test script.

--- src/tests/run-check-dsstatgroupby.sh
+++ src/tests/run-check-dsstatgroupby.sh
@@ -8,10 +8,10 @@
 
 set -e 
 
-../process/dsstatgroupby 'I/O' basic '1000*(return_to_driver - leave_driver)' group by 'device_number' basic 'bytes/1024' group by 'machine_id' from ~/projects/DataSeries/check-data/h03126.ds-littleend >test.dsstatgroupby-tmp
+../process/dsstatgroupby 'I/O' basic '1000*(return_to_driver - leave_driver)' group by 'device_number' basic 'bytes/1024' group by 'machine_id' from $1/check-data/h03126.ds-littleend >test.dsstatgroupby-tmp
 perl $1/check-data/clean-timing.pl <test.dsstatgroupby-tmp >test.dsstatgroupby.1
 cmp test.dsstatgroupby.1 $1/check-data/test.dsstatgroupby.1.ref
-../process/dsstatgroupby 'Batch::LSF' basic 'start_time - submit_time' where 'start_time - submit_time > 50000' group by 'production' basic 'cpu_time/(end_time-start_time)' group by production quantile 'start_time - submit_time' where 'start_time - submit_time > 50000' group by production from ~/projects/DataSeries/check-data/lsb.acct.2007-01-01-p1.ds >test.dsstatgroupby.tmp
+../process/dsstatgroupby 'Batch::LSF' basic 'start_time - submit_time' where 'start_time - submit_time > 50000' group by 'production' basic 'cpu_time/(end_time-start_time)' group by production quantile 'start_time - submit_time' where 'start_time - submit_time > 50000' group by production from $1/check-data/lsb.acct.2007-01-01-p1.ds >test.dsstatgroupby.tmp
 perl $1/check-data/clean-timing.pl <test.dsstatgroupby.tmp >test.dsstatgroupby.2
 cmp test.dsstatgroupby.2 $1/check-data/test.dsstatgroupby.2.ref
 rm test.dsstatgroupby.tmp
