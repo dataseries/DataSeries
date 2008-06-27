b16c4b5f57a62db64070670c53c729de3267bca5
Eric Anderson <eric.anderson4@hp.com>
2008-06-27 01:42:29 +0000

    [BUGFIX]: the regression files for the expr tests didn't run their output   through clean-timing, so different machines could get different results.   Updated regression tests to pass on more machines.
    clean-timing.pl: accept no space after the wait fraction so when we finally
      fix that weirdness it will keep working.
    
    expr-*.txt: update all of the outputs to remove the strange space
    
    CMakeLists.txt: comment on the different sections of the cmakefile, move
      the expr tests into the always run section.
    
    run-check-expr.sh: run all the outputs through clean-timing.  Add TODO about 
      collapsing all of these tests into a single one at some point since
      dsstatgroupby can calculate multiple stats in a single pass.

--- check-data/clean-timing.pl
+++ check-data/clean-timing.pl
@@ -3,7 +3,7 @@ use strict;
 while(<STDIN>) {
     s/ \d+\.\d{2} secs decode time/ 0.00 secs decode time/o;
     s/decode seconds: +\d+\.\d{2}/decode seconds:      0.00/o;
-    s/wait fraction +: +\d+\.\d{2}/wait fraction:       0.00/o;
+    s/wait fraction *: +\d+\.\d{2}/wait fraction:       0.00/o;
     s/wait fraction *: +\d+\.\d{2} +\d+\.\d{2}/wait fraction:       0.00     0.00/o;
     print;
 }
--- src/tests/run-check-expr.sh
+++ src/tests/run-check-expr.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 #
 # (c) Copyright 2008, Hewlett-Packard Development Company, LP
 #
@@ -8,23 +8,30 @@
 
 set -e
 
+SRC=$1
+
 ./expr
-../process/ds2txt --skip-index expr.ds >expr.txt
+../process/ds2txt --skip-index expr.ds >expr.tmp
+perl $SRC/check-data/clean-timing.pl <expr.tmp >expr.txt
 
 run() {
-  ../process/dsstatgroupby 'Test::Simple' basic a where "$1" group by b basic b where "$1" group by a from expr.ds > "$2"
+  ../process/dsstatgroupby 'Test::Simple' basic a where "$1" group by b basic b where "$1" group by a from expr.ds > "$2".tmp
+  perl $SRC/check-data/clean-timing.pl <"$2".tmp >"$2".txt
 }
 
-run "a != b" expr-neq.txt
-run "a == b" expr-eq.txt
-run "a > b" expr-gt.txt
-run "a < b" expr-lt.txt
-run "a >= b" expr-geq.txt
-run "a <= b" expr-leq.txt
+# TODO: collapse all of these into a single test run with a single output file
+# and do all the comparisons in one go.  Leads to fewer regression test files.
+
+run "a != b" expr-neq
+run "a == b" expr-eq
+run "a > b" expr-gt
+run "a < b" expr-lt
+run "a >= b" expr-geq
+run "a <= b" expr-leq
 
 for x in '' '-neq' '-eq' '-gt' '-lt' '-geq' '-leq'; do
     echo $x
-    cmp $1/check-data/expr$x.txt expr$x.txt
+    cmp $SRC/check-data/expr$x.txt expr$x.txt
 done
 
 #rm expr.ds expr.txt expr-neq.txt expr-eq.txt expr-gt.txt \
