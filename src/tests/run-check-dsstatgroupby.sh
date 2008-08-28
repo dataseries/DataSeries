9fc9db735b6e3f725f04c3ffced02cd9444803f8
Eric Anderson <eric.anderson4@hp.com>
2008-08-28 05:35:44 +0000

    test.dsstatgroupby.2.ref: The quantile changes in lintel also affected   this test.
    run-check-dsstatgroupby.sh: since we have a script to do an unordered
      compare of files, we don't need to sort the output file, which makes it
      much easier to read.

--- src/tests/run-check-dsstatgroupby.sh
+++ src/tests/run-check-dsstatgroupby.sh
@@ -13,7 +13,7 @@ perl $1/check-data/clean-timing.pl <test.dsstatgroupby-tmp | sort >test.dsstatgr
 perl $1/check-data/unordered-file-equality.pl test.dsstatgroupby.1 $1/check-data/test.dsstatgroupby.1.ref
 
 ../process/dsstatgroupby 'Batch::LSF' basic 'start_time - submit_time' where 'start_time - submit_time > 50000' group by 'production' basic 'cpu_time/(end_time-start_time)' group by production quantile 'start_time - submit_time' where 'start_time - submit_time > 50000' group by production from $1/check-data/lsb.acct.2007-01-01-p1.ds >test.dsstatgroupby.tmp
-perl $1/check-data/clean-timing.pl <test.dsstatgroupby.tmp | sort >test.dsstatgroupby.2
+perl $1/check-data/clean-timing.pl <test.dsstatgroupby.tmp >test.dsstatgroupby.2
 perl $1/check-data/unordered-file-equality.pl test.dsstatgroupby.2 $1/check-data/test.dsstatgroupby.2.ref
 rm test.dsstatgroupby.tmp
 
