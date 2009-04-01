80b4d0f1dcf1eeb4290ec33464445330486bf0ed
Tomer Shiran <tomer.shiran@hp.com>
2009-04-01 17:46:46 +0000

    Cleaned up fgrepanalysis and added fgrepanalysis.py for easy measurements

---
+++ src/analysis/fgrep/fgrepanalysis.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <string.h>
+
+#include <DataSeries/RowAnalysisModule.hpp>
+#include <DataSeries/SimpleSourceModule.hpp>
+#include <DataSeries/TypeIndexModule.hpp>
+#include <DataSeries/ExtentField.hpp>
+
+#include "BoyerMooreHorspool.hpp"
+
+using namespace std;
+
+class FgrepAnalysisModule: public RowAnalysisModule {
+public:
+    FgrepAnalysisModule(DataSeriesModule &source, const string &pattern, bool print_matches=false);
+
+    virtual void processRow() {
+        ++line_number;
+        const char *val = (const char*)line.val(); // not NULL-terminated
+        int32_t size = line.size();
+
+        if (matcher.matches(val, size)) {
+            ++match_count;
+            if (print_matches) {
+                cout << std::string((char*)val, size)  << endl;
+            }
+        }
+    }
+
+    virtual void printResult() {
+        cout << "*** Found " << match_count << ((match_count == 1) ? " match " : " matches") << " in " << line_number << " lines (" << extent_count << " extents)" << endl;
+    }
+
+    virtual ~FgrepAnalysisModule();
+
+    virtual void newExtentHook(const Extent &e);
+
+private:
+        Variable32Field line;
+
+        string pattern;
+        int64_t line_number;
+        int32_t match_count;
+        int32_t extent_count;
+        bool print_matches;
+        BoyerMooreHorspool matcher;
+};
+
+FgrepAnalysisModule::FgrepAnalysisModule(DataSeriesModule &source, const string &pattern, bool print_matches) :
+        RowAnalysisModule(source),
+        line(series, "line"),
+        pattern(pattern),
+        line_number(0),
+        match_count(0),
+        extent_count(0),
+        print_matches(print_matches),
+        matcher(pattern.c_str(), pattern.size()) {
+}
+
+FgrepAnalysisModule::~FgrepAnalysisModule() {
+}
+
+void FgrepAnalysisModule::newExtentHook(const Extent &e) {
+    ++extent_count;
+}
+
+void printUsage(const char *command) {
+    cerr << "Usage: " << command << " [--no-memcpy] [--count] pattern filename" << endl <<
+            "    --no-memcpy: Use the specialized SimpleSourceModule module for reading the (uncompressed) file" << endl <<
+            "    --count: Suppress normal output; instead print a count of matching lines" << endl <<
+            "    pattern: A fixed string that the fgrep module will search for" << endl <<
+            "    filename: A DS file that was created via txt2ds (and --compress-none if --no-memcpy is used)" << endl;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        printUsage(argv[0]);
+        return 1;
+    }
+
+    bool no_memcpy = false;
+    bool count = false;
+    const char *pattern = argv[argc - 2];
+    const char *filename = argv[argc - 1];
+
+    for (int i = 1; i < argc - 2; ++i) {
+        string argument(argv[i]);
+        if (argument == "--no-memcpy") {
+            no_memcpy = true;
+        } else if (argument == "--count") {
+            count = true;
+        } else {
+            printUsage(argv[0]);
+            return 2;
+        }
+    }
+
+    DataSeriesModule *source = NULL;
+    if (no_memcpy) {
+        source = new SimpleSourceModule(filename);
+    } else {
+        source = new TypeIndexModule("Text"); // "Text" is the type name used by ds2text
+        static_cast<TypeIndexModule*>(source)->addSource(filename);
+    }
+
+    FgrepAnalysisModule analysis(*source, pattern, !count);
+    analysis.getAndDelete();
+    analysis.printResult();
+    delete source;
+
+    return 0;
+}
---
+++ src/analysis/fgrep/fgrepanalysis.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+
+from subprocess import call
+import sys	
+import os
+import re
+import getopt
+
+class Measurements:
+	def __init__(self, parameters, skipFirst):
+		self.parameters = parameters
+		self.total = dict([(name, 0.0) for name in parameters])
+		self.skip = skipFirst
+		self.count = 0
+		self.min = dict([(name, float('inf')) for name in parameters])
+		self.max = dict([(name, 0.0) for name in parameters])
+		
+	def record(self, results):
+		if self.skip:
+			self.skip = False
+			return
+
+		for (key, val) in results.iteritems():
+			self.total[key] += val
+			self.min[key] = min(self.min[key], val)
+			self.max[key] = max(self.max[key], val)
+		self.count += 1
+	
+	def mean(self):
+		return dict([
+			[key, val / self.count] for (key, val) in self.total.iteritems()
+			])
+	
+	def getResultList(self, d):
+		return ["%s" % d[key] for key in self.parameters]
+		
+	def __str__(self):
+		return " ".join(self.parameters) + "\n" + \
+			"Mean: " + " ".join(self.getResultList(self.mean())) + "\n" + \
+			"Min: " + " ".join(self.getResultList(self.min)) + "\n" + \
+			"Max: " + " ".join(self.getResultList(self.max))
+		
+
+
+def runText(command):
+	(stdinHandle, stdoutHandle, stderrHandle) = os.popen3(command)
+	return (stdoutHandle.read(), stderrHandle.read())
+
+def runLines(command):
+	(stdinHandle, stdoutHandle, stderrHandle) = os.popen3(command)
+	return (stdoutHandle.readlines(), stderrHandle.readlines())
+
+def run(command):
+	os.popen(command)
+
+def extractTime(expression, stderrText):
+	return expression.search(stderrText).group(2)
+
+def measure(command):
+	(stdoutLines, stderrLines) = runLines('/usr/bin/time -f "%e %U %S %P" ' + command)
+	line = stderrLines[-1]
+	components = line.split()
+	return {'elapsed': float(components[0]), 'user': float(components[1]), 'system': float(components[2]), 'cpu': float(components[3][0:-1])}
+
+def printUsage():
+	print """Usage: %(command)s <command>
+Various commands are supported:
+1) --prepare [--copies=copies] <input text file> <output text file> <output DS file>
+2) --experiment [--iterations=iterations [--skip-first-iteration]] <pattern> <text file> <DS file>
+   (grep uses the text file, fgrepanalysis uses the equivalent DS file)
+
+Examples:
+%(command)s --prepare --copies=200 /usr/share/datasets/fgrepbible.txt /usr/share/datasets/fgrepbible_200_none.txt /usr/share/datasets/fgrepbible_200_none.in
+%(command)s --experiment --copies=200 "seven" /usr/share/datasets/fgrepbible_200_none.txt /usr/share/datasets/fgrepbible_200_none.in""" % {'command': sys.argv[0]}
+
+def exitUsage(retvalue):
+	printUsage()
+	sys.exit(retvalue)
+
+def prepare(inputTextFile, outputTextFile, outputDsFile, copies):
+	print "=====> Creating text file '%s'" % outputTextFile
+	runText('cat %s > %s' % (inputTextFile, outputTextFile))
+	for i in range(copies - 1):
+		runText('cat %s >> %s' % (inputTextFile, outputTextFile))
+	
+	print "=====> Creating DataSeries file '%s' via 'txt2ds --compress-none'" % outputDsFile
+	run("txt2ds --compress-none %s %s %s" % (inputTextFile, outputDsFile, copies))
+
+def experiment(pattern, textFile, dsFile, iterations, skipFirstIteration):
+	grepMeasurements = Measurements(['elapsed', 'user', 'system', 'cpu'], skipFirstIteration)
+	dsMeasurements = Measurements(['elapsed', 'user', 'system', 'cpu'], skipFirstIteration)
+	dsNoMemcpyMeasurements = Measurements(['elapsed', 'user', 'system', 'cpu'], skipFirstIteration)
+		
+	print "=====> Measuring performance of grep on %s" % textFile
+	for i in range(iterations):
+		grepMeasurements.record(measure("grep --count %s %s" % (pattern, textFile)))
+	print grepMeasurements
+	
+	print "=====> Measuring performance of 'fgrepanalysis' on %s" % dsFile
+	for i in range(iterations):
+		dsMeasurements.record(measure("fgrepanalysis --count %s %s" % (pattern, dsFile)))
+	print dsMeasurements
+	
+	print "=====> Measuring performance of 'fgrepanalysis --no-memcpy' on %s" % dsFile
+	for i in range(iterations):
+		dsNoMemcpyMeasurements.record(measure("fgrepanalysis --count --no-memcpy %s %s" % (pattern, dsFile)))
+	print dsNoMemcpyMeasurements
+
+if __name__ == "__main__":
+	try:                                
+		opts, args = getopt.getopt(sys.argv[1:], "", ["prepare", "experiment", "copies=", "iterations=", "skip-first-iteration"])
+		opts = dict(opts)
+	except getopt.GetoptError, err:
+		print "ERROR: %s" % err
+		printUsage()
+		sys.exit(2)
+		
+	if '--prepare' in opts:
+		if len(args) != 3:
+			exitUsage(3)
+		args.append(int(opts.get('--copies', '1')))
+		prepare(*args)
+	elif '--experiment' in opts:
+		if len(args) != 3:
+			exitUsage(3)
+		args.append(int(opts.get('--iterations', '1')))
+		args.append(opts.get('--skip-first-iteration') is not None);
+		experiment(*args)
+	else:
+		exitUsage(3)
