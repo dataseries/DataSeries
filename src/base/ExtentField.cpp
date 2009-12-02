ceba47fa7bcb047b8f8fedb8a8f7c6d7ee826c18
Hernan Laffitte <hernan.laffitte@hp.com>
2009-12-02 01:07:36 +0000

    fixed small bug that shows up with LINTEL_DEBUG turned on that was not using rounded up byte addressed memory and thus was underflowing when data_size was not a multiple of 4.  Resulted in an infinite loop.

--- src/base/ExtentField.cpp
+++ src/base/ExtentField.cpp
@@ -207,7 +207,7 @@ void Variable32Field::allocateSpace(uint32_t data_size) {
 #if LINTEL_DEBUG
     // we get to avoid zeroing since it happens automatically for us
     // when we resize the bytearray
-    for(++var_data; data_size > 0; data_size -= 4) {
+    for(++var_data; roundup > 0; roundup -= 4) {
 	SINVARIANT(*var_data == 0);
     }
 #endif
