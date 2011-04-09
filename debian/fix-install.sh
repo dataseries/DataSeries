03a830d79ed8b4d55901bc5ded6c20679142230f
Eric Anderson <eric.anderson4@hp.com>
2011-04-09 04:32:59 +0000

    * Final bits to make redhat packages pass rpmlint
    fix-install.sh: make sure to strip dynamically linked programs
    
    DataSeries.spec.in: add TODO's, skip putting dataseries-config into utils also.

--- debian/fix-install.sh
+++ debian/fix-install.sh
@@ -6,10 +6,11 @@ TMP_USR=$1/usr
 # Unclear whether lexgrog or doxygen is wrong, but lexgrog does not accept spaces 
 # on the left hand side of the NAME entry.
 find $TMP_USR/share/man/man3 -type f -print0 | xargs -0 perl -i -p -e 'if ($fix) { ($left, $right) = /^(.+)( \\-.+)$/; $left =~ s/ //go; $_ = "$left$right\n"; $fix = 0; }; $fix = 1 if /^.SH NAME/o;' 
+
 for i in $TMP_USR/bin/* $TMP_USR/lib/libDataSeries.so.*; do
     TYPE=`file $i`
     case "$TYPE" in
-        *dynamically*linked*) chrpath -d $i ;;
+        *dynamically*linked*) chrpath -d $i; strip $i ;;
         *ELF*LSB*shared*object*) chrpath -d $i; strip $i ;;
         *shell*script*|*perl*script*) : ;;
         *symbolic*link*to*) : ;;
