500e7d947cf6f53380a150d17938f10fccb28c93
Eric Anderson <eric.anderson4@hp.com>
2012-03-02 16:40:58 -0800

    changelog, DataSeries.spec.in: fake entry for release testing
    fix-install.sh: Add the same TODO as was in Lintel, need to verify it isn't happenning here either.
    
    make-tar.sh: Switch to the git variant of tar creation; fix the same bugs as were fixed in the
      Lintel version.

--- debian/fix-install.sh
+++ debian/fix-install.sh
@@ -3,6 +3,8 @@ set -e
 [ -d "$1" ]
 TMP_USR=$1/usr
 
+echo "Figure out how to make the perl stuff not include my path in the release (if happening)"
+exit 1
 # Unclear whether lexgrog or doxygen is wrong, but lexgrog does not accept spaces 
 # on the left hand side of the NAME entry.
 find $TMP_USR/share/man/man3 -type f -print0 | xargs -0 perl -i -p -e 'if ($fix) { ($left, $right) = /^(.+)( \\-.+)$/; $left =~ s/ //go; $_ = "$left$right\n"; $fix = 0; }; $fix = 1 if /^.SH NAME/o;' 
--- redhat/make-tar.sh
+++ redhat/make-tar.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e -x
 case `pwd` in
    */DataSeries | */DataSeries.0.20[0-9][0-9].[0-9][0-9].[0-9][0-9])
       : 
@@ -8,11 +9,11 @@ case `pwd` in
       ;;
 esac
 
-if [ -d _MTN ]; then
+if [ -d .git ]; then
     `dirname $0`/../../Lintel/dist/make-release-changelog.sh
 fi
 
-if [ ! -f Release.info -o ! -f Changelog.mtn ]; then
+if [ ! -f Release.info -o ! -f ChangeLog ]; then
     echo "Error: Ought to either both Release.info and Changelog.mtn from a release or should have just created it from monotone repository"
     exit 1
 fi
@@ -22,8 +23,10 @@ fi
 cwd=`pwd`
 dir=`basename $cwd`
 cd ..
-[ "$dir" == "DataSeries-$VERSION" ] || ln -s $dir DataSeries-$VERSION
-tar cvvfhz DataSeries-$VERSION.tar.gz --exclude=DataSeries-$VERSION/_MTN DataSeries-$VERSION/
-[ "$dir" == "DataSeries-$VERSION" ] || rm DataSeries-$VERSION
+[ "$dir" = "DataSeries-$VERSION" -o -d DataSeries-$VERSION ] || ln -s $dir DataSeries-$VERSION
+tar cvvfhz DataSeries-$VERSION.tar.gz --exclude=DataSeries-$VERSION/.git DataSeries-$VERSION/
+[ "$dir" = "DataSeries-$VERSION" ] || rm DataSeries-$VERSION
+
+mkdir -p $rpm_topdir/SOURCES
 cp DataSeries-$VERSION.tar.gz $rpm_topdir/SOURCES/DataSeries-$VERSION.tar.gz
 
